d9171fc62acc8e848387a774e8531f78a2de9f54
Otto <51783206+HEPOSHEIKKI@users.noreply.github.com>
2022-08-11 11:58:00 +0300

    Add files via upload

--- tavainstall.sh
+++ tavainstall.sh
@@ -21,6 +21,10 @@ for i in "$@"; do
 			echo "-h (--help): 				Printib välja selle abiteksti inglise keeles"
 			exit 1
 			;;
+		-l|--language)
+			english=1
+			shift
+			;;
     -*|--*)
       echo "Unknown option $i"
       exit 1
@@ -29,50 +33,92 @@ for i in "$@"; do
       ;;
   esac
 done
-if [ "$EUID" -ne 0 ]
-  then echo "palun jooksuta skript SUDOna (sudo ./tavainstall.sh)"
-  exit
-fi
-echo "Kas lubad firefoxi SNAPi versiooni desinstallimise?"
-echo "Palun kirjuta 'JAH', või 'EI"
-read consent
-if [ $consent = "EI" ]; then
-	echo "Skript peatatud, firefoxi desinstallimist ei lubatud."
+if [ $english = 1 ]; then
+	if [ "$EUID" -ne 0 ]
+	  then echo "Please run the script as root (sudo ./tavainstall.sh)"
+	  exit
+	fi
+	echo "Firefox snap will now be uninstalled, is that okay?"
+	echo "Please type YES or NO"
+	read consent
+	if [ $consent != "YES" ]; then
+		echo "Skript peatatud, firefoxi desinstallimist ei lubatud."
+		exit
+	fi
+	echo "Removing firefox"
+	sudo snap remove firefox
+	mkdir ./tulerebane
+	cd ./tulerebane
+	echo "Downloading firefox version-103"
+	echo "Please wait"
+	wget https://download-installer.cdn.mozilla.net/pub/firefox/releases/103.0.2/linux-x86_64/en-US/firefox-103.0.2.tar.bz2
+	echo "Extracting the firefox archive"
+	echo "Please wait"
+	sudo tar -xf firefox-*.tar.bz2
+	echo "Finishing up"
+	echo "Please wait"
+	sudo mv firefox /opt
+	sudo ln -s /opt/firefox/firefox /usr/local/bin/firefox
+	sudo wget https://raw.githubusercontent.com/mozilla/sumo-kb/main/install-firefox-linux/firefox.desktop -P /usr/local/share/applications
+	echo "Firefox is now installed"
+	echo "Do you wish to install the eID extension for firefox?"
+	echo "JAH/EI:"
+	read consent
+	if [ $consent = "YES" ]; then
+		if [ ! -d "/opt/firefox/extensions/" ]; then
+			mkdir /opt/firefox/extensions/
+		fi
+		if [ ! -d "/opt/firefox/extensions/{e68418bc-f2b0-4459-a9ea-3e72b6751b07}/" ]; then
+			mkdir /opt/firefox/extensions/{e68418bc-f2b0-4459-a9ea-3e72b6751b07}/
+		fi
+		wget https://addons.mozilla.org/firefox/downloads/file/3963431/web_eid_webextension-2.1.1.xpi -P /opt/firefox/extensions/{e68418bc-f2b0-4459-a9ea-3e72b6751b07}/
+		wget https://addons.mozilla.org/firefox/downloads/file/3439907/pkcs11_module_loader-1.0.5.xpi -P /opt/firefox/extensions/{02274e0c-d135-45f0-8a9c-32b35110e10d}/
+		echo "eID installed"
+	fi
+	echo "Good bye!"
+	exit
+else
+		if [ "$EUID" -ne 0 ]
+	  then echo "palun jooksuta skript SUDOna (sudo ./tavainstall.sh)"
+	  exit
+	fi
+	echo "Kas lubad firefoxi SNAPi versiooni desinstallimise?"
+	echo "Palun kirjuta 'JAH', või 'EI"
+	read consent
+	if [ $consent != "JAH" ]; then
+		echo "Skript peatatud, firefoxi desinstallimist ei lubatud."
+		exit
+	fi
+	echo "Kustutan firefoxi"
+	sudo snap remove firefox
+	mkdir ./tulerebane
+	cd ./tulerebane
+	echo "Laetagse alla firefoxi versioon-103"
+	echo "Palun oota"
+	wget https://download-installer.cdn.mozilla.net/pub/firefox/releases/103.0.2/linux-x86_64/en-US/firefox-103.0.2.tar.bz2
+	echo "Pakin firefoxi arhhiivi lahti"
+	echo "Palun oota"
+	sudo tar -xf firefox-*.tar.bz2
+	echo "Viimistlen firefoxi installatsiooni"
+	echo "Palun oota"
+	sudo mv firefox /opt
+	sudo ln -s /opt/firefox/firefox /usr/local/bin/firefox
+	sudo wget https://raw.githubusercontent.com/mozilla/sumo-kb/main/install-firefox-linux/firefox.desktop -P /usr/local/share/applications
+	echo "Firefox on nüüd installitud"
+	echo "Kas soovite ka eID plugini installida?"
+	echo "JAH/EI:"
+	read consent
+	if [ $consent = "JAH" ]; then
+		if [ ! -d "/opt/firefox/extensions/" ]; then
+			mkdir /opt/firefox/extensions/
+		fi
+		if [ ! -d "/opt/firefox/extensions/{e68418bc-f2b0-4459-a9ea-3e72b6751b07}/" ]; then
+			mkdir /opt/firefox/extensions/{e68418bc-f2b0-4459-a9ea-3e72b6751b07}/
+		fi
+		wget https://addons.mozilla.org/firefox/downloads/file/3963431/web_eid_webextension-2.1.1.xpi -P /opt/firefox/extensions/{e68418bc-f2b0-4459-a9ea-3e72b6751b07}/
+		wget https://addons.mozilla.org/firefox/downloads/file/3439907/pkcs11_module_loader-1.0.5.xpi -P /opt/firefox/extensions/{02274e0c-d135-45f0-8a9c-32b35110e10d}/
+		echo "eID installitud"
+	fi
+	echo "Head aega!"
 	exit
 fi
-echo "Kustutan firefoxi"
-sudo snap remove firefox
-mkdir ./tulerebane
-cd ./tulerebane
-echo "Laetagse alla firefoxi versioon-103"
-echo "Palun oota"
-wget https://download-installer.cdn.mozilla.net/pub/firefox/releases/103.0.2/linux-x86_64/en-US/firefox-103.0.2.tar.bz2
-echo "Pakin firefoxi arhhiivi lahti"
-echo "Palun oota"
-sudo tar -xf firefox-*.tar.bz2
-echo "Viimistlen firefoxi installatsiooni"
-echo "Palun oota"
-sudo mv firefox /opt
-sudo ln -s /opt/firefox/firefox /usr/local/bin/firefox
-sudo wget https://raw.githubusercontent.com/mozilla/sumo-kb/main/install-firefox-linux/firefox.desktop -P /usr/local/share/applications
-echo "Firefox on nüüd installitud"
-echo "Kas soovite ka eID plugini installida?"
-echo "JAH/EI:"
-read consent
-if [ $consent = "JAH" ]; then
-	if [ ! -d "/usr/share/mozilla/" ]; then
-		mkdir /usr/share/mozilla/
-	fi
-	if [ ! -d "/usr/share/mozilla/extensions/" ]; then
-		mkdir /usr/share/mozilla/extensions/
-	fi
-	if [ ! -d "/usr/share/mozilla/extensions/{e68418bc-f2b0-4459-a9ea-3e72b6751b07}/" ]; then
-		mkdir /usr/share/mozilla/extensions/{e68418bc-f2b0-4459-a9ea-3e72b6751b07}/
-	fi
-	wget https://addons.mozilla.org/firefox/downloads/file/3963431/web_eid_webextension-2.1.1.xpi -P /usr/share/mozilla/extensions/{e68418bc-f2b0-4459-a9ea-3e72b6751b07}/
-	mkdir /usr/share/mozilla/extensions/{02274e0c-d135-45f0-8a9c-32b35110e10d}/
-	wget https://addons.mozilla.org/firefox/downloads/file/3439907/pkcs11_module_loader-1.0.5.xpi -P /usr/share/mozilla/extensions/{02274e0c-d135-45f0-8a9c-32b35110e10d}/
-	echo "eID installitud"
-fi
-echo "Head aega!"
-exit
