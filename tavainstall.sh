c53f0ef5bb03f270a29467ca55eb6367dc01574e
Otto <51783206+HEPOSHEIKKI@users.noreply.github.com>
2022-08-11 16:10:15 +0300

    Fized
    Tried to undo the damage github's web editor did to the file

--- tavainstall.sh
+++ tavainstall.sh
@@ -67,8 +67,8 @@ if [[ $english = YES ]]; then
 	sudo wget https://raw.githubusercontent.com/mozilla/sumo-kb/main/install-firefox-linux/firefox.desktop -P $USER_HOME/Desktop/
 	sudo chown $USER_HOME/Desktop/firefox.desktop $SUDO_USER:$SUDO_USER
 	sudo chmod +x $USER_HOME/Desktop/firefox.desktop
-	echo "Firefox is generating the user configuration, please wait (4s)"
-	timeout 4s su $SUDO_USER -c "firefox
+	echo "Firefox is generating the user configuration, please wait (10s)"
+	timeout 10s su $SUDO_USER -c "firefox --headless"
 	echo "Firefox is now installed"
 	echo "Do you wish to install the eID extension for firefox?"
 	echo "YES/NO:"
@@ -76,7 +76,8 @@ if [[ $english = YES ]]; then
 	if [ $consent = "YES" ]; then
 		USER="$(ls $USER_HOME/.mozilla/firefox/ | grep -v ".default-release-1" | grep default-release)"
 		sudo touch $USER_HOME/.mozilla/firefox/$USER/user.js
-		sudo echo echo "pref(\"extensions.autoDisableScopes\", 0); >> $USER_HOME/.mozilla/firefox/$USER/user.js"
+		sudo echo "pref("extensions.autoDisableScopes", 0);" >> $USER_HOME/.mozilla/firefox/$USER/user.js
+		sudo mkdir $USER_HOME/.mozilla/firefox/$USER/extensions
 		wget https://addons.mozilla.org/firefox/downloads/file/3963431/web_eid_webextension-2.1.1.xpi -O $USER_HOME/.mozilla/firefox/$USER/extensions/{e68418bc-f2b0-4459-a9ea-3e72b6751b07}.xpi
 		wget https://addons.mozilla.org/firefox/downloads/file/3439907/pkcs11_module_loader-1.0.5.xpi -O $USER_HOME/.mozilla/firefox/$USER/extensions/{02274e0c-d135-45f0-8a9c-32b35110e10d}.xpi
 		echo "eID installed"
@@ -84,9 +85,9 @@ if [[ $english = YES ]]; then
 	echo "Good bye!"
 	exit
 else
-		if [ "$EUID" -ne 0 ]; then
-			echo "palun jooksuta skript SUDOna (sudo ./tavainstall.sh)"
-	  	exit
+		if [ "$EUID" -ne 0 ]
+	  then echo "palun jooksuta skript SUDOna (sudo ./tavainstall.sh)"
+	  exit
 	fi
 	USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
 	echo "Kas lubad firefoxi SNAPi versiooni desinstallimise?"
@@ -104,11 +105,11 @@ else
 	echo "Palun oota"
 	sleep 2s
 	wget https://download-installer.cdn.mozilla.net/pub/firefox/releases/103.0.2/linux-x86_64/en-US/firefox-103.0.2.tar.bz2
+	rm firefox-103.0.2.tar.bz2
 	sleep 2s
 	echo "Pakin firefoxi arhhiivi lahti"
 	echo "Palun oota"
 	sudo tar -xf firefox-*.tar.bz2
-	rm firefox-103.0.2.tar.bz2
 	echo "Viimistlen firefoxi installatsiooni"
 	echo "Palun oota"
 	sudo mv firefox /opt
@@ -117,8 +118,8 @@ else
 	sudo wget https://raw.githubusercontent.com/mozilla/sumo-kb/main/install-firefox-linux/firefox.desktop -P $USER_HOME/Desktop/
 	sudo chown $USER_HOME/Desktop/firefox.desktop $SUDO_USER:$SUDO_USER
 	sudo chmod +x $USER_HOME/Desktop/firefox.desktop
-	echo "Firefox genereerib kasutajakonfiguratsiooni, palun oota (4s)"
-	timeout 4s su $SUDO_USER -c "firefox"
+	echo "Firefox genereerib kasutajakonfiguratsiooni, palun oota (10s)"
+	timeout 10s su $SUDO_USER -c "firefox"
 	echo "Firefox on nüüd installitud"
 	echo "Kas soovite ka eID plugini installida?"
 	echo "JAH/EI:"
@@ -126,9 +127,10 @@ else
 	if [ $consent = "JAH" ]; then
 		USER="$(ls $USER_HOME/.mozilla/firefox/ | grep -v ".default-release-1" | grep default-release)"
 		sudo touch $USER_HOME/.mozilla/firefox/$USER/user.js
-		sudo echo "pref(\"extensions.autoDisableScopes\", 0); >> $USER_HOME/.mozilla/firefox/$USER/user.js"
-		wget https://addons.mozilla.org/firefox/downloads/file/3963431/web_eid_webextension-2.1.1.xpi -O $USER_HOME/.mozilla/firefox/$USER/{e68418bc-f2b0-4459-a9ea-3e72b6751b07}.xpi
-		wget https://addons.mozilla.org/firefox/downloads/file/3439907/pkcs11_module_loader-1.0.5.xpi -O $USER_HOME/.mozilla/firefox/$USER/{02274e0c-d135-45f0-8a9c-32b35110e10d}.xpi
+		sudo echo "pref("extensions.autoDisableScopes", 0);" >> $USER_HOME/.mozilla/firefox/$USER/user.js
+		sudo mkdir $USER_HOME/.mozilla/firefox/$USER/extensions
+		wget https://addons.mozilla.org/firefox/downloads/file/3963431/web_eid_webextension-2.1.1.xpi -O $USER_HOME/.mozilla/firefox/$USER/extensions/{e68418bc-f2b0-4459-a9ea-3e72b6751b07}.xpi
+		wget https://addons.mozilla.org/firefox/downloads/file/3439907/pkcs11_module_loader-1.0.5.xpi -O $USER_HOME/.mozilla/firefox/$USER/extensions/{02274e0c-d135-45f0-8a9c-32b35110e10d}.xpi
 		echo "eID installed"
 	fi
 	echo "Head aega!"
